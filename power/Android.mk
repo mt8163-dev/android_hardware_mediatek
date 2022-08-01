80628b502e52e57c6acb1cbfc200a5ab48f84979
bengris32 <bengris32@protonmail.ch>
2022-08-01 23:30:05 +0200

    mediatek: Rewrite legacy power hal as hidl service

--- power/Android.mk
+++ power/Android.mk
@@ -1,35 +1,27 @@
-# Copyright (C) 2011 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := power.c
-
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := android.hardware.power@1.3-service.mt8163
+LOCAL_INIT_RC := android.hardware.power@1.3-service.mt8163.rc
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_VENDOR_MODULE := true
 
-LOCAL_SHARED_LIBRARIES := liblog libutils
-LOCAL_HEADER_LIBRARIES := libhardware_headers
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/include
 
-LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_OWNER := mtk
+LOCAL_SRC_FILES := \
+    service.cpp \
+    Power.cpp
 
-ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
-  LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
-endif
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libutils \
+    libhidlbase \
+    libhidltransport \
+    android.hardware.power@1.0 \
+    android.hardware.power@1.1 \
+    android.hardware.power@1.2 \
+    android.hardware.power@1.3
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_EXECUTABLE)
